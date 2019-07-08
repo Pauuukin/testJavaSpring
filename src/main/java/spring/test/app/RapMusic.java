04bd5d104b0a0a14881eb3855c5f226a3accfdc1
Pauuukin <oleg.paukin@gmail.com>
2019-07-08 18:57:41 +0300

    add list realisation

---
+++ src/main/java/spring/test/app/RapMusic.java
@@ -0,0 +1,9 @@
+package spring.test.app;
+
+public class RapMusic implements Music {
+
+    @Override
+    public String getSong(){
+        return "Rap God";
+    }
+}
--- src/main/java/spring/test/app/TestSpring.java
+++ src/main/java/spring/test/app/TestSpring.java
@@ -11,8 +11,9 @@ public class TestSpring {
 
         //MusicPlayer musicPlayer = new MusicPlayer(music);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println();
+        musicPlayer.playMusicList();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
